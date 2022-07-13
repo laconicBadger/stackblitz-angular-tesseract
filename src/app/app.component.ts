2660d5b303d6d016e51278c4de25ffee045f2add
laconicBadger <273.15@tutanota.com>
2022-07-13 16:40:22 +0200

    1 binaries uploaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,22 +4,30 @@ import { createWorker } from 'tesseract.js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //template: `
+  //  Hello!
+  //`,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'tesseract.js-angular-app';
+  title = 'Tesseract Angular';
   ocrResult = 'Recognizing...';
   constructor() {
     this.doOCR();
   }
   async doOCR() {
     const worker = createWorker({
-      logger: m => console.log(m),
+      logger: (m) => console.log(m),
     });
     await worker.load();
     await worker.loadLanguage('eng');
     await worker.initialize('eng');
-    const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+    debugger;
+    const {
+      data: { text },
+    } = await worker.recognize(
+      'https://tesseract.projectnaptha.com/img/eng_bw.png'
+    );
     this.ocrResult = text;
     console.log(text);
     await worker.terminate();
